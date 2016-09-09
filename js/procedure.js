08fe61600d8179a5b44a4e3abb969d1dc2011e5a
John Karasinski <karasinski@gmail.com>
2016-09-09 16:06:24 -0700

    restructure data; add a video link

--- js/procedure.js
+++ js/procedure.js
@@ -1,135 +1,231 @@
 var proc = {
   "PROCEDURE": [
-    ["1.  REMOVE AIR CLEANER ASSEMBLY", [
-      ["1.1.    SWITCH ENGINE SWITCH → OFF See Figure 1.", [
-        ["Check ENGINE SWITCH – OFF (Audible Call Out)"],
-        ['/img/figures/Fig1.png']
-      ]],
-      ["1.2.    Unplug all power cords from CONTROL PANEL", [
-        ["Check CONTROL PANEL – Cleared (Audible Call Out)"]
-      ]],
-      ["1.3.    SWITCH FUEL BREATHER VALVE LEVER → OFF", [
-        ["Check FUEL BREATHER VALVE LEVER – OFF (Audible Call Out)"],
-      ]],
-      ["1.4.    Remove the Spark Plug Cover located on top of Generator and remove the Spark Plug Cap. See Figure 2.", [
-        ['/img/figures/Fig2.png']
-      ]],
-      ["1.5.    Loosen the Cover Screw and remove the Maintenance Cover and set aside [Flat Head Screwdriver] See Figure 3.", [
-        ['/img/figures/Fig3.png']
-      ]],
-      ["1.6.    Loosen the Air Cleaner Cover Screw (Bin # ______) and remove \n         ☐ Air Cleaner Cover \n         ☐ Outer Filter (Small, lower filter) \n         ☐ Main Filter (Large, upper filter) \n [Flat Head Screwdriver]. See Figure 4.", [
-        ['/img/figures/Fig4.png']
-      ]],
-      ["CAUTION: Pry the Breather Tube off gently to avoid damaging it."],
-      ["1.7.    Remove the end of Breather Tube which is attached to the Air Cleaner Case. See Figure 5. [Flat Head Screwdriver, Rag, Pliers].", [
-        ['/img/figures/Fig5.png']
-      ]],
-      ["1.8.    Loosen, remove, and store \n         ☐ Center 8mm Bolt \n         ☐ 8mm flange Nuts (two) \n         ☐ Bin # ______ \n from the Air Cleaner Case, and then remove the Air Cleaner Case [Ratcheting Socket Wrench and Socket Set, 1/4\" Drive; 8mm Hex Socket]."],
-      ["1.9.    Remove the Air Intake Joint Gasket \n         ☐ Bin # ______"]
-    ]],
-    ["2.  CARBURETOR REMOVAL", [
-      ["2.1.    Clamp the Carburetor Fuel Tube [Hose Pinching Pliers]."],
-      ["2.2.    Put Gasoline Container under the Carburetor Drain Tube."],
-      ["2.3.    Loosen, but do not remove, Carburetor Drain Screw and drain fuel into Gasoline Container [Flat Head Screwdriver]. (Audible Call Out)"],
-      ["2.4.    Tighten the Carburetor Drain Screw securely. [1/8\" Flat Head Screwdriver]"],
-      ["2.5.    Remove Gasoline Container from under the Carburetor Drain Tube."],
-      ["CAUTION: Pry the Carburetor Fuel Tube off gently to avoid damaging it."],
-      ["2.6.    Remove Carburetor Fuel Tube Clamp [Pliers]."],
-      ["2.7.    Detach Carburetor Fuel Tube from Carburetor [Flat Head Screwdriver]."],
-      ["2.8.    Check that the tube routing configuration matches Figures 6 and 7. (Audible Call Out)", [
-        ['/img/figures/Fig6.png'],
-        ['/img/figures/Fig7.png']
-      ]],
-      ["2.9.    Remove Carburetor Drain Tube", [
-        ["2.9.1.  Pull Carburetor Drain Tube upwards from red plastic housing until bottom end is free"],
-        ["2.9.2.  Remove tube from fitting at bottom of Carburetor Bowl"],
-        ['/img/figures/Fig8.png']
-      ]],
-      ["2.10.   Remove Right Hand Vent Tube from Carburetor. See Figure 8."],
-      ["2.11.   Remove Left Hand Vent Tube from Carburetor. See Figure 9.", [
-        ['/img/figures/Fig9.png']
-      ]],
-      ["2.12.   Remove Tube Assembly"],
-      ["2.13.   Remove Carburetor by sliding it off of the bolts attaching it to the main unit."]
-    ]],
-    ["3.  FLOAT CHAMBER INSPECTION", [
-      ["3.1.    Remove the \n         ☐ 10mm Bolt \n         ☐ Washer \n         ☐ Bin # ______ \n That secures the float chamber to the Carburetor Assembly [Ratcheting Socket Wrench and Socket Set, 1/4\" Drive; 10 mm Hex Bit].", [
-        ['/img/figures/Fig10.png']
-      ]],
-      ["3.2.    Remove Float Chamber (See Figure 10) and Float Chamber Gasket from the Carburetor Assembly. [Pry Tool] Bin # ______"],
-      ["3.3.    Remove Float Securing Pin [Small Needle Nose Pliers] Bin # ______"],
-      ["3.4.    Remove Float"],
-      ["3.5.    Inspect Float Chamber for Deposits and Debris. (Audible Call Out)"],
-      ["3.6.    Inspect per Figure 11 for wear and operation. See Figure 11. (Audible Call Out):", [
-        ["3.6.1.  Float Valve Tip"],
-        ["3.6.2.  Float Valve Spring"],
-        ['/img/figures/Fig11.png']
-      ]]
-    ]],
-    ["4.  FLOAT CHAMBER REASSEMBLY", [
-      ["4.1.    Replace and secure Float with Float Securing Pin. Bin # from Step 3.3"],
-      ["4.2.    Put \n         ☐ Float Chamber Gasket \n         ☐ Float Chamber \n in place. Bin # from Step 3.2"],
-      ["4.3.    Secure Float Chamber to Carburetor Body with bolt and washer. Bin # from Step 3.1. Match the Index Mark on the Float Chamber Flange. Tighten the bolt securely. [Ratcheting Socket Wrench and Socket Set, 1/4\" Drive; 10 mm Hex Bit]."]
-    ]],
-    ["5.  CARBURETOR REPLACEMENT", [
-      ["5.1.    Replace the Carburetor by sliding it onto the bolts attaching it to the main unit.", [
-        ['/img/figures/Fig12.png']
-      ]],
-      ["5.2.    Reposition Vent Tube Assembly. See Figure 12.", [
-        ["5.2.1.  Reattach Right Hand Vent Tube to Carburetor"],
-        ["5.2.2.  Reattach Left Hand Vent Tube to Carburetor"],
-        ["5.2.3.  Reposition Bottom of Vent Tube Assembly into right bottom hole"]
-      ]],
-      ["5.3.    Replace Carburetor Fuel Tube."],
-      ["5.4.    Slide clamp into position on the Carburetor Fuel Tube [Pliers]."],
-      ["5.5.    Remove Hose Pinching Pliers."],
-      ["5.6.    Reattach Carburetor Drain Tube", [
-        ["5.6.1.  Reattach Carburetor Drain Tube to bottom of Carburetor Bowl"],
-        ["5.6.2.  Push bottom of Carburetor Drain Tube through left bottom hole until it is visible from outside."]
-      ]],
-      ["5.7.    Check that the routing configuration matches Figure 12. (Audible Call Out)"],
-      ["5.8.    Inspect bottom surface for gasoline or tools; clean if necessary. (Audible Call Out)"]
-    ]],
-    ["6.  REPLACE AIR CLEANER ASSEMBLY", [
-      ["6.1.    Replace Air Intake Joint Gasket. Bin # from Step 1.9"],
-      ["6.2.    Replace the Air Cleaner Case. Refer to Step 1.8 for Bin # containing parts and then replace and tighten \n         ☐ Center 8mm Bolt \n         ☐ 8mm flange Nuts (two) \n on the Air Cleaner Case [Ratcheting Socket Wrench and Socket Set, 1/4\" Drive; 8mm Hex Socket]."],
-      ["6.3.    Reattach the detached end of the Breather Tube to the Air Cleaner Case"],
-      ["6.4.    Replace items in Bin # from Step 1.6 \n         ☐ Main Filter (Large, upper filter) \n         ☐ Outer Filter (Small, lower filter) \n         ☐ Air Cleaner Cover"],
-      ["6.5.    Tighten the Air Cleaner Cover Screw [Flat Head Screwdriver]"],
-      ["6.6.    Replace the Maintenance Cover and tighten the Cover Screw securing the Maintenance Cover [Flat Head Screwdriver]."],
-      ["6.7.    Replace \n         ☐ Spark Plug Cap \n         ☐ Spark Plug Cover"]
-    ]]
+    {
+      "text": "1.  REMOVE AIR CLEANER ASSEMBLY",
+      "subtasks": [{
+        "text": "1.1.    SWITCH ENGINE SWITCH → OFF See Figure 1.",
+        "subtasks": [{
+          "text": "Check ENGINE SWITCH – OFF (Audible Call Out)"
+        }, {
+          "text": "/img/figures/Fig1.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "1.2.    Unplug all power cords from CONTROL PANEL",
+        "subtasks": [{
+          "text": "Check CONTROL PANEL – Cleared (Audible Call Out)"
+        }]
+      }, {
+        "text": "1.3.    SWITCH FUEL BREATHER VALVE LEVER → OFF",
+        "subtasks": [{
+          "text": "Check FUEL BREATHER VALVE LEVER – OFF (Audible Call Out)"
+        }]
+      }, {
+        "text": "1.4.    Remove the Spark Plug Cover located on top of Generator and remove the Spark Plug Cap. See Figure 2.",
+        "subtasks": [{
+          "text": "/img/figures/Fig2.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "1.5.    Loosen the Cover Screw and remove the Maintenance Cover and set aside [Flat Head Screwdriver] See Figure 3.",
+        "subtasks": [{
+          "text": "/img/figures/Fig3.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "1.6.    Loosen the Air Cleaner Cover Screw (Bin # ______) and remove \n         ☐ Air Cleaner Cover \n         ☐ Outer Filter (Small, lower filter) \n         ☐ Main Filter (Large, upper filter) \n [Flat Head Screwdriver]. See Figure 4.",
+        "subtasks": [{
+          "text": "/img/figures/Fig4.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "CAUTION: Pry the Breather Tube off gently to avoid damaging it.",
+        "type": "caution"
+      }, {
+        "text": "1.7.    Remove the end of Breather Tube which is attached to the Air Cleaner Case. See Figure 5. [Flat Head Screwdriver, Rag, Pliers].",
+        "subtasks": [{
+          "text": "/img/figures/Fig5.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "1.8.    Loosen, remove, and store \n         ☐ Center 8mm Bolt \n         ☐ 8mm flange Nuts (two) \n         ☐ Bin # ______ \n from the Air Cleaner Case, and then remove the Air Cleaner Case [Ratcheting Socket Wrench and Socket Set, 1/4\" Drive; 8mm Hex Socket]."
+      }, {
+        "text": "1.9.    Remove the Air Intake Joint Gasket \n         ☐ Bin # ______"
+      }]
+    },
+
+    {
+      "text": "2.  CARBURETOR REMOVAL",
+      "subtasks": [{
+        "text": "2.1.    Clamp the Carburetor Fuel Tube [Hose Pinching Pliers]."
+      }, {
+        "text": "2.2.    Put Gasoline Container under the Carburetor Drain Tube."
+      }, {
+        "text": "2.3.    Loosen, but do not remove, Carburetor Drain Screw and drain fuel into Gasoline Container [Flat Head Screwdriver]. (Audible Call Out)"
+      }, {
+        "text": "2.4.    Tighten the Carburetor Drain Screw securely. [1/8\" Flat Head Screwdriver]"
+      }, {
+        "text": "2.5.    Remove Gasoline Container from under the Carburetor Drain Tube."
+      }, {
+        "text": "CAUTION: Pry the Carburetor Fuel Tube off gently to avoid damaging it.",
+        "type": "caution"
+      }, {
+        "text": "2.6.    Remove Carburetor Fuel Tube Clamp [Pliers]."
+      }, {
+        "text": "2.7.    Detach Carburetor Fuel Tube from Carburetor [Flat Head Screwdriver]."
+      }, {
+        "text": "2.8.    Check that the tube routing configuration matches Figures 6 and 7. (Audible Call Out)",
+        "subtasks": [{
+          "text": "/img/figures/Fig6.png",
+          "type": "image"
+        }, {
+          "text": "/img/figures/Fig7.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "2.9.    Remove Carburetor Drain Tube",
+        "subtasks": [{
+          "text": "2.9.1.  Pull Carburetor Drain Tube upwards from red plastic housing until bottom end is free"
+        }, {
+          "text": "2.9.2.  Remove tube from fitting at bottom of Carburetor Bowl"
+        }, {
+          "text": "/img/figures/Fig8.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "2.10.   Remove Right Hand Vent Tube from Carburetor. See Figure 8."
+      }, {
+        "text": "2.11.   Remove Left Hand Vent Tube from Carburetor. See Figure 9.",
+        "subtasks": [{
+          "text": "/img/figures/Fig9.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "2.12.   Remove Tube Assembly"
+      }, {
+        "text": "2.13.   Remove Carburetor by sliding it off of the bolts attaching it to the main unit."
+      }]
+    },
+
+    {
+      "text": "3.  FLOAT CHAMBER INSPECTION",
+      "subtasks": [{
+        "text": "3.1.    Remove the \n         ☐ 10mm Bolt \n         ☐ Washer \n         ☐ Bin # ______ \n That secures the float chamber to the Carburetor Assembly [Ratcheting Socket Wrench and Socket Set, 1/4\" Drive; 10 mm Hex Bit].",
+        "subtasks": [{
+          "text": "/img/figures/Fig10.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "3.2.    Remove Float Chamber (See Figure 10) and Float Chamber Gasket from the Carburetor Assembly. [Pry Tool] Bin # ______"
+      }, {
+        "text": "3.3.    Remove Float Securing Pin [Small Needle Nose Pliers] Bin # ______"
+      }, {
+        "text": "3.4.    Remove Float"
+      }, {
+        "text": "3.5.    Inspect Float Chamber for Deposits and Debris. (Audible Call Out)"
+      }, {
+        "text": "3.6.    Inspect per Figure 11 for wear and operation. See Figure 11. (Audible Call Out):",
+        "subtasks": [{
+          "text": "3.6.1.  Float Valve Tip"
+        }, {
+          "text": "3.6.2.  Float Valve Spring"
+        }, {
+          "text": "/img/figures/Fig11.png",
+          "type": "image"
+        }]
+      }]
+    },
+
+    {
+      "text": "4.  FLOAT CHAMBER REASSEMBLY",
+      "subtasks": [{
+        "text": "4.1.    Replace and secure Float with Float Securing Pin. Bin # from Step 3.3"
+      }, {
+        "text": "4.2.    Put \n         ☐ Float Chamber Gasket \n         ☐ Float Chamber \n in place. Bin # from Step 3.2"
+      }, {
+        "text": "4.3.    Secure Float Chamber to Carburetor Body with bolt and washer. Bin # from Step 3.1. Match the Index Mark on the Float Chamber Flange. Tighten the bolt securely. [Ratcheting Socket Wrench and Socket Set, 1/4\" Drive; 10 mm Hex Bit]."
+      }]
+    },
+
+    {
+      "text": "5.  CARBURETOR REPLACEMENT",
+      "subtasks": [{
+        "text": "5.1.    Replace the Carburetor by sliding it onto the bolts attaching it to the main unit.",
+        "subtasks": [{
+          "text": "/img/figures/Fig12.png",
+          "type": "image"
+        }]
+      }, {
+        "text": "5.2.    Reposition Vent Tube Assembly. See Figure 12.",
+        "subtasks": [{
+          "text": "5.2.1.  Reattach Right Hand Vent Tube to Carburetor"
+        }, {
+          "text": "5.2.2.  Reattach Left Hand Vent Tube to Carburetor"
+        }, {
+          "text": "5.2.3.  Reposition Bottom of Vent Tube Assembly into right bottom hole"
+        }]
+      }, {
+        "text": "5.3.    Replace Carburetor Fuel Tube."
+      }, {
+        "text": "5.4.    Slide clamp into position on the Carburetor Fuel Tube [Pliers]."
+      }, {
+        "text": "5.5.    Remove Hose Pinching Pliers."
+      }, {
+        "text": "5.6.    Reattach Carburetor Drain Tube",
+        "subtasks": [{
+          "text": "5.6.1.  Reattach Carburetor Drain Tube to bottom of Carburetor Bowl"
+        }, {
+          "text": "5.6.2.  Push bottom of Carburetor Drain Tube through left bottom hole until it is visible from outside."
+        }]
+      }, {
+        "text": "5.7.    Check that the routing configuration matches Figure 12. (Audible Call Out)"
+      }, {
+        "text": "5.8.    Inspect bottom surface for gasoline or tools; clean if necessary. (Audible Call Out)"
+      }]
+    },
+
+    {
+      "text": "6.  REPLACE AIR CLEANER ASSEMBLY",
+      "subtasks": [{
+        "text": "6.1.    Replace Air Intake Joint Gasket. Bin # from Step 1.9"
+      }, {
+        "text": "6.2.    Replace the Air Cleaner Case. Refer to Step 1.8 for Bin # containing parts and then replace and tighten \n         ☐ Center 8mm Bolt \n         ☐ 8mm flange Nuts (two) \n on the Air Cleaner Case [Ratcheting Socket Wrench and Socket Set, 1/4\" Drive; 8mm Hex Socket]."
+      }, {
+        "text": "6.3.    Reattach the detached end of the Breather Tube to the Air Cleaner Case"
+      }, {
+        "text": "6.4.    Replace items in Bin # from Step 1.6 \n         ☐ Main Filter (Large, upper filter) \n         ☐ Outer Filter (Small, lower filter) \n         ☐ Air Cleaner Cover"
+      }, {
+        "text": "6.5.    Tighten the Air Cleaner Cover Screw [Flat Head Screwdriver]"
+      }, {
+        "text": "6.6.    Replace the Maintenance Cover and tighten the Cover Screw securing the Maintenance Cover [Flat Head Screwdriver]."
+      }, {
+        "text": "6.7.    Replace \n         ☐ Spark Plug Cap \n         ☐ Spark Plug Cover"
+      }, {
+        "text": "<iframe width='560' height='315' src='https://www.youtube.com/embed/uDnqoF6Qvf4' frameborder='0' allowfullscreen></iframe>",
+        "type": "video"
+      }]
+    }
   ]
 }
 
-var procedure = proc['PROCEDURE'];
-$.each(procedure, function(key, section) {
-  var sectionID = 'section_' + key;
+function buildProcedure(steps, parent, depth) {
+  steps.forEach(function(step, index) {
+    var procID = parent + '_' + index
 
-  var $section = $('<li id="' + sectionID + '">' + section[0] + '</li>').appendTo('#expList')
-  $('<ul class="steps"></ul').appendTo('li#' + sectionID);
-
-  $.each(section[1], function(key, step) {
-    var stepID = 'step_' + key;
-
-    var $step = $('<li id="' + stepID + '">' + step[0] + '</li>').appendTo('li#' + sectionID + ' ul.steps')
-    $('<ul class="substeps"></ul').appendTo('li#' + sectionID + ' ul li#' + stepID);
-
-    try {
-      $.each(step[1], function(key, substep) {
-        var substepID = 'substep_' + key;
-        if (substep[0][0] == '/') {
-          console.log('image!!', substep[0])
-          var $substep = $('<li id="' + substepID + '"><img class="proc_image" src="' + substep[0].substr(1) + '"></li>').appendTo('#' + sectionID + ' #' + stepID + ' ul')
-        } else {
-          var $substep = $('<li id="' + substepID + '">' + substep[0] + '</li>').appendTo('#' + sectionID + ' #' + stepID + ' ul')
-        }
-      })
-    } catch (e) {}
+    if (step.type == 'image') {
+      $('<li id="' + procID + '">' + '<img class="proc_image" src="' + step.text.substr(1) + '"></li>' + '</li>').appendTo('ul#' + parent)
+    } else if (step.type == 'video') {
+      $('<li id="' + procID + '">' + step.text + '</li>' + '</li>').appendTo('ul#' + parent)
+    } else if (step.type == 'caution') {
+      $('<li id="' + procID + '" class="caution">' + step.text + '</li>').appendTo('ul#' + parent)
+    } else {
+      $('<li id="' + procID + '">' + step.text + '</li>').appendTo('ul#' + parent)
+    }
+    if (step.subtasks != undefined) {
+      $('<ul id="' + procID + '"></ul').appendTo('li#' + procID);
+      buildProcedure(step.subtasks, procID, depth + 1)
+    }
   })
-})
+}
 
-// Some steps don't actually _have_ substeps, let's remove them
-$('ul').filter(function() {
-  return $.trim(this.innerHTML) === ""
-}).remove()
+buildProcedure(proc['PROCEDURE'], 'expList', 0)
